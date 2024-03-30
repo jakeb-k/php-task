e8acf6bc422a6aa2112ece39ca09ab50761621ec
jakeb-k <jknow30@outlook.com>
2024-03-30 14:15:50 +1000

    create table function

--- php-task.php
+++ php-task.php
@@ -1,7 +1,6 @@
 #!/usr/bin/php
 <?php 
 
-$file = fopen("users.csv","r");
 
 $values = []; 
 
@@ -12,18 +11,56 @@ $password = $options['p'] ?? "";
 $host = $options['h']; 
 
 
-if(isset($username) && isset($password) && isset($host)) {
-    echo 'success'; 
-} else {
-    echo fwrite(STDOUT, "Error: missing required values for db \nPlease enter the required values using the specified directives \nRun with --help flag for instructions\n");
-    exit(1);
+function main(){
+    $file = fopen("users.csv","r");
+ 
+    while (($data = fgetcsv($file)) !== FALSE)
+    {
+        $x = [name_check($data[0]), name_check($data[1]), email_check($data[2])];
+        $values[] = $x; 
+    }
+    
 }
-#$con = mysqli_connect('localhost','root','');
 
-while (($data = fgetcsv($file)) !== FALSE)
-{
-    $x = [name_check($data[0]), name_check($data[1]), email_check($data[2])];
-    $values[] = $x; 
+function create_table() {
+    //Check DB variables are properly set
+    if(isset($username) && isset($password) && isset($host)) {
+        echo 'success'; 
+    } else {
+        echo fwrite(STDOUT, "Error: missing required values for db \nPlease enter the required values using the specified directives \nRun with --help flag for instructions\n");
+        exit(1);
+    }
+
+    //create the connection and check for error
+    $con = mysqli_connect('localhost','root','', 'task_db'); 
+    if($con->connect_error) {
+        die("Connection failed: ".$con->connect_error );
+    }
+    
+    //remove previous table and create new one
+    $create_query = "DROP TABLE IF EXISTS users;
+    CREATE TABLE IF NOT EXISTS users(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        surname VARCHAR(50) NOT NULL,
+        email VARCHAR(128) NOT NULL UNIQUE
+    );";
+
+    //try the query and output if success or not
+    try {
+        $success = $con->query($create_query); 
+        if($success) {
+            echo 'Table users was created successfully';
+        } else {
+            echo "Error creating table: ". $con->error; 
+        }
+    } //catch unforeseen errors
+    catch (Exception $e) {
+        echo "Error : ". $e; 
+    }
+
+    //close the connection
+    $con->close(); 
 }
 
 function name_check($name) {
@@ -38,4 +75,5 @@ function email_check($email) {
     $email = filter_var($email, FILTER_VALIDATE_EMAIL); 
     return $email; 
 }
+
 ?>
