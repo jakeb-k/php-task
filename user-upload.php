860a69587adb0271510bf94de475adf0d1180951
jakeb-k <jknow30@outlook.com>
2024-03-30 15:47:39 +1000

    help flag function

--- user-upload.php
+++ user-upload.php
@@ -6,12 +6,31 @@ $options = getopt("u:p:h", ["file","create_table","dry_run","help"]);
 $username = $options['u'] ?? "root";
 $password = $options['p'] ?? "";
 $host = $options['h'] ?? "localhost";  
+$fileName = $options['file'] ?? "users.csv"; 
 
+if(isset($options['help'])) {
+    help();
+    die(0); 
+}
 
 create_table(); 
 insert_values();  
 
+function help(){
+    echo "
+    • --file [csv file name] - this is the name of the CSV to be parsed
+    • --create_table - this will cause the MySQL users table to be built (and no further action will be taken)
+    • --dry_run - this will be used with the --file directive in case we want to run the script but not insert 
+    into the DB. All other functions will be executed, but the database won't be altered
+    • -u - MySQL username
+    • -p - MySQL password
+    • -h - MySQL host
+    • --help - which will output the above list of directives with details.\n"; 
+}
+
+
 function input_validation(){
+    
     $file = fopen("users.csv","r");
     //skip header row 
     fgetcsv($file);
