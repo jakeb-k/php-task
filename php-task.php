5d8f69bb17902b19ecac2e828b924e5654b969e7
jakeb-k <jknow30@outlook.com>
2024-03-30 13:35:03 +1000

    cli directives init

--- php-task.php
+++ php-task.php
@@ -5,14 +5,26 @@ $file = fopen("users.csv","r");
 
 $values = []; 
 
+$options = getopt("u:p:h", ["file","create_table","dry_run","help"]);
+
+$username = $options['u'];
+$password = $options['p'] ?? "";
+$host = $options['h']; 
+
+
+if(isset($username) && isset($password) && isset($host)) {
+    echo 'success'; 
+} else {
+    echo fwrite(STDOUT, "Error: missing required values for db \nPlease enter the required values using the specified directives \nRun with --help flag for instructions\n");
+    exit(1);
+}
+#$con = mysqli_connect('localhost','root','');
+
 while (($data = fgetcsv($file)) !== FALSE)
 {
-    $email = $data[2]; 
-
     $x = [name_check($data[0]), name_check($data[1]), email_check($data[2])];
     $values[] = $x; 
 }
-print_r($values); 
 
 function name_check($name) {
     $name = preg_replace('/\PL/u', '', $name); 
