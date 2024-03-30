4ae326e9c76910059c906239c279ed8f57f22fbe
jakeb-k <jknow30@outlook.com>
2024-03-30 13:04:10 +1000

    email validation + assign to values arr

--- php-task.php
+++ php-task.php
@@ -3,17 +3,27 @@
 
 $file = fopen("users.csv","r");
 
+$values = []; 
 
 while (($data = fgetcsv($file)) !== FALSE)
 {
-    echo "name: " . name_check($data[0])."\n";
+    $email = $data[2]; 
+
+    $x = [name_check($data[0]), name_check($data[1]), email_check($data[2])];
+    $values[] = $x; 
 }
+print_r($values); 
 
 function name_check($name) {
     $name = preg_replace('/\PL/u', '', $name); 
     $name = strtolower($name);
     $name = ucfirst($name); 
-
     return $name; 
+}
+
+function email_check($email) {
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $email = filter_var($email, FILTER_VALIDATE_EMAIL); 
+    return $email; 
 }
 ?>
