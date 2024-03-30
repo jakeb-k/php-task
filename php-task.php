15e5c53929eefb2c4057f071b36410e6fe02f87a
jakeb-k <jknow30@outlook.com>
2024-03-30 12:41:13 +1000

    name validation

--- php-task.php
+++ php-task.php
@@ -6,7 +6,14 @@ $file = fopen("users.csv","r");
 
 while (($data = fgetcsv($file)) !== FALSE)
 {
-    echo "email: " . $data[2]."\n";
+    echo "name: " . name_check($data[0])."\n";
 }
 
+function name_check($name) {
+    $name = preg_replace('/\PL/u', '', $name); 
+    $name = strtolower($name);
+    $name = ucfirst($name); 
+
+    return $name; 
+}
 ?>
